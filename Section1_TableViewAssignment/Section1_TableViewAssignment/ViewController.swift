8c4d7a3f74cc74e7e03d66b0b98ba99a4545f0ed
Azamat Kenjebayev <azamatk@me.com>
2022-10-03 11:44:50 +0600

    Add files via upload

--- Section1_TableViewAssignment/Section1_TableViewAssignment/ViewController.swift
+++ Section1_TableViewAssignment/Section1_TableViewAssignment/ViewController.swift
@@ -9,23 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let countries: [String] = ["USA", "Canada", "Kazakhstan", "Mexico", "China", "Australia", "Turkey", "United Kingdom", "Germany", "Poland", "Austria", "France", "Portugal", "Spain", "Norway", "Sweden", "Italy", "UAE", "Egypt", "Greece", "Finland", "New Zealand", "Vietnam", "Malaysia", "Singapore", "India", "Pakistan", "Thailand", "Brazil", "Argentina"]
-    private let countriesPopulation: [String] = ["338.2M", "38.4M", "19.3M", "127.5M", "1.425B", "26.1M", "85.3M", "67.5M", "83.3M", "39.8M", "8.9M", "64.6M", "10.2M", "47.5M", "5.4M", "10.5M", "59M", "9.4M", "110.9M", "10.3M", "5.5M", "5.1M", "98.1M", "33.9M", "5.9M", "1.417B", "235.8M", "71.6M", "215.3M", "45.5M"]
+    private let countries:[String] = ["China",
+                                      "India",
+                                      "United States",
+                                      "Indonesia",
+                                      "Pakistan",
+                                      "Nigeria",
+                                      "Brazil",
+                                      "Bangladesh",
+                                      "Russia",
+                                      "Mexico",
+                                      "Japan",
+                                      "Ethiopia",
+                                      "Philippines",
+                                      "Egypt",
+                                      "DR Congo",
+                                      "Vietnam",
+                                      "Iran",
+                                      "Turkey",
+                                      "Germany",
+                                      "Thailand",
+                                      "United Kingdom",
+                                      "Tanzania",
+                                      "France",
+                                      "South Africa",
+                                      "Italy",
+                                      "England",
+                                      "Myanmar",
+                                      "Kenya",
+                                      "Colombia",
+                                      "South Korea"]
+    private let countriesPopulation:[String] = ["1425887.3370",
+                                                "1417173.1730",
+                                                "338289.8570",
+                                                "275501.3390",
+                                                "235824.8620",
+                                                "218541.2120",
+                                                "215313.4980",
+                                                "171186.3720",
+                                                "144713.3140",
+                                                "127504.1250",
+                                                "123951.6920",
+                                                "123379.9240",
+                                                "115559.0090",
+                                                "110990.1030",
+                                                "99010.2120",
+                                                "98186.8560",
+                                                "88550.5700",
+                                                "85341.2410",
+                                                "83369.8430",
+                                                "71697.0300",
+                                                "67508.9360",
+                                                "65497.7480",
+                                                "64626.6280",
+                                                "59893.8850",
+                                                "59037.4740",
+                                                "57367.8110",
+                                                "54179.3060",
+                                                "54027.4870",
+                                                "51874.0240",
+                                                "51815.8100"]
     
-    @IBOutlet var countryTableView: UITableView!
+    private var cellIdentifier = "CellIdentifier"
     
-    private let cellIdentifier = "CellIdentifier"
+    @IBOutlet var countriesTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        countryTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        countriesTable.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
+
 
 }
 
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return countries.count
     }
@@ -35,18 +93,18 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
-        let country = countries[indexPath.row]
-        let countryPopulation = countriesPopulation[indexPath.row]
+        let countries = countries[indexPath.row]
+        let countriesPopulation = countriesPopulation[indexPath.row]
         
         var configuration = cell.defaultContentConfiguration()
-        configuration.text = country
-        configuration.secondaryText = countryPopulation
-        
+        configuration.text = countries
+        configuration.secondaryText = countriesPopulation
         cell.contentConfiguration = configuration
         
         return cell
     }
-    
 }
+
---
+++ Section1_TableViewAssignment/Section1_TableViewAssignmentUITests/Section1_TableViewAssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Section1_TableViewAssignmentUITestsLaunchTests.swift
+//  Section1_TableViewAssignmentUITests
+//
+//  Created by Azamat Kenjebayev on 10/3/22.
+//
+
+import XCTest
+
+final class Section1_TableViewAssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
